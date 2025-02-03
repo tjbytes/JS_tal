9cd75a700dce2901c95539bac650916365b7a219
Tal <taljacobi94@gmail.com>
2025-02-04 00:00:22 +0200

    Update

--- JS_1/hw_page27_ex.7/index.js
+++ JS_1/hw_page27_ex.7/index.js
@@ -1,1 +1,14 @@
-let 
+let number = +prompt("Enter a number:");
+let max = -1;
+
+while (number > 0) {
+  if (number > max) {
+    max = number;
+  }
+  number = +prompt("Enter a number:");
+}
+  if (max === -1) {
+    console.log("Enter some positive numbers");
+  } else {
+    console.log(`The highest number entered is: ${max}`);
+  }
