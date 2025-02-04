4b107538eb0add4fa195a5b345c485070b14a917
Tal <taljacobi94@gmail.com>
2025-02-04 17:09:31 +0200

    Update

--- JS_2/hw_page29_ex.28/index.js
+++ JS_2/hw_page29_ex.28/index.js
@@ -1,15 +1,26 @@
-let arr = []
-let sum = 0
+let arr2000 = [];
+let arr2001 = [];
+let sum = 0;
 
-for (i = 1; i <= 30; i++) {
-arr.push(+prompt("Enter the daily temperature of July 2000:"))
+for (i = 1; i <= 5; i++) {
+  arr2000.push(+prompt("Enter the daily temperature of July 2000:"));
 }
 
-for (i = 0; i < arr.length; i++) {
-    console.log(arr[i])
-    sum = sum + arr[i]
+for (i = 0; i < arr2000.length; i++) {
+  arr2000[i];
+  sum = sum + arr2000[i];
 }
 
-let average = sum / arr.length
+let average = sum / arr2000.length;
 
-console.log(`The daily average temperature in July 2000 was: ${average}`)
+console.log(`The daily average temperature in July 2000 was: ${average}°C`);
+
+for (i = 1; i <= 5; i++) {
+  arr2001.push(+prompt("Enter the daily temperature of July 2001:"));
+}
+
+for (i = 0; i < arr2001.length; i++) {
+  if (arr2001[i] > average) {
+    console.log(`Day ${i + 1}: ${arr2001[i]}°C`);
+  }
+}
